28ca4d39c58850161358681520e251a506355475
GitCesc07 <cesar04071997@gmail.com>
2023-02-07 14:07:52 -0600

    Agregando mensajes en el HTML, y luego validaciones para que nos lleve al apartado del index.html

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -35,6 +35,39 @@
 
             return;
         }
+
+        // Crear un objeto con la información
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa,
+            // id: Date.now()
+        }
+
+        cliente.id = Date.now(); // Esto es igual, como que el valor este dentro del parametro cliente
+
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente (cliente) {
+        const transaction = DB.transaction(["crm"], "readwrite");
+
+        const objectStore = transaction.objectStore("crm");
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function() {
+            imprimirAlerta("Hubo un error...", "error");
+        };
+
+        transaction.oncomplete = function() {
+            imprimirAlerta("Cliente agregado correctamente...");
+
+            setTimeout(() => {
+                window.location.href = "index.html";
+            }, 3000);
+        };
     }
 
     function imprimirAlerta(mensaje, tipo) {
@@ -45,18 +78,18 @@
             // Crear alerta
             const divMensaje = document.createElement("div");
             divMensaje.classList.add("px-4", "py-3", "rounded", "max-w-lg", "mx-auto", "mt-6", "text-center", "border", "alerta");
-            
+
             if(tipo === "error") {
                 divMensaje.classList.add("bg-red-100", "border-red-400", "text-red-700");
             }
             else {
                 divMensaje.classList.add("bg-green-100", "border-green-400", "text-green-700");
             }
-        
+
             divMensaje.textContent = mensaje;
-        
+
             formulario.appendChild(divMensaje);
-        
+
             setTimeout(() => {
                 divMensaje.remove();
             }, 3000);
