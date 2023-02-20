5ea212331148256e744c55d4b3891522270bd9f5
GitCesc07 <cesar04071997@gmail.com>
2023-02-20 13:01:38 -0600

    Agregando la información de la base de datos, a la tabla

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,21 +1,65 @@
 (function () {
     let DB;
 
+    // Variables globales
+    const nombreInput = document.querySelector("#nombre");
+    const emailInput = document.querySelector("#email");
+    const telefonoInput = document.querySelector("#telefono");
+    const empresaInput = document.querySelector("#empresa");
+
+    const formulario = document.querySelector("#formulario");
+
     document.addEventListener("DOMContentLoaded", () => {
 
         conectarDB();
+
+        // Actualiza el registro
+        formulario.addEventListener("submit", actualizarCliente);
+
 
         // verificar el ID de la Url
         const parametrosUrl = new URLSearchParams(window.location.search);
 
         const idCliente = parametrosUrl.get("id");
         if(idCliente) {
+         setTimeout(() => {
             obtenerCliente(idCliente);
+         }, 100);
         }
     });
 
+    function actualizarCliente(e) {
+        e.preventDefault();
+
+        if(nombreInput.value === "" || emailInput.value === "" || telefonoInput.value === "" || empresaInput.value === "") {
+            
+        }
+    }
+
     function obtenerCliente(id) {
         const transaction = DB.transaction(["crm"], "readwrite");
+        const objectStore = transaction.objectStore("crm");
+
+        const cliente = objectStore.openCursor();
+        cliente.onsuccess = function (e) {
+            const cursor = e.target.result;
+
+            if(cursor) {
+                if(cursor.value.id === Number(id)) {
+                    llenarFormulario(cursor.value);
+                }
+                cursor.continue();
+            }
+        }
+    }
+
+    function llenarFormulario(datosClientes) {
+        const { nombre, email, telefono, empresa } = datosClientes;
+
+        nombreInput.value = nombre;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        empresaInput.value = empresa;
     }
 
     function conectarDB() {
